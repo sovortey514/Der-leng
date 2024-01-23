1de0d64e5a49134cfa7560e51918f43bf6f9a134
hbheng <hb988651@gmail.com>
2024-01-23 16:48:43 +0700

    finish review Crud

--- backend/derleng/urls.py
+++ backend/derleng/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     path('', include(viewsetRouter.urls), name='viewset'),
     path('thumbnails' , ThumbnailView.ThumbnailAPIView.as_view() , name=''),
     path('thumbnails/<uuid:pk>' , ThumbnailView.ThumbnailAPIView.as_view() , name=''),
-    path('category' , CategoryView.CategoryAPIView.as_view(), name=''),
-    path('category/<uuid:pk>' , CategoryView.CategoryAPIView.as_view(), name=''),
-    path('review' , ReviewView.ReviewAPIView.as_view(), name=''),
-    path('review/<uuid:pk>' , ReviewView.ReviewAPIView.as_view(), name=''),
+    path('categorys' , CategoryView.CategoryAPIView.as_view(), name=''),
+    path('categorys/<uuid:pk>' , CategoryView.CategoryAPIView.as_view(), name=''),
+    path('reviews' , ReviewView.ReviewAPIView.as_view(), name=''),
+    path('reviews/<uuid:pk>' , ReviewView.ReviewAPIView.as_view(), name='review-detail'),
     path('test-payment/', PaymentView.test_payment, name="test_payment"),
 ]
--- backend/derleng/views/ReviewView.py
+++ backend/derleng/views/ReviewView.py
@@ -5,26 +5,68 @@ from rest_framework.response import Response
 from derleng.models import *
 from derleng.serializers import *
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated , IsAuthenticatedOrReadOnly
+# from authentication.permissions import Is_Admin_Or_Staff_Or_TourGuide_Or_ReadOnly
 
 
 
 class ReviewAPIView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     def post(self  , request , *args, **kwargs):
-        if request.user.is_authenticated:
-            data = request.data
+        try:
+            print(request.user)
+            data = request.data.copy()
+            data['user'] = request.user.id
+            data['package'] = data['package_id']
+            print(data)
             serializers = ReviewSerializer(data=data)
-            if serializers.is_valid():
-                review = Review (
-                    user_id = request.user,
-                    package_id  = data.get('package_id'),
-                    rating = data.get('rating'),
-                    comment = data.get('comment'),
-                )
-                review.save()
-                
-                respone_data = {
-                    'message' : 'Review post successfully.' ,
+            serializers.is_valid(raise_exception=True)
+            serializers.save()
+            
+            respone_data = {
+                'message' : 'Review post successfully.' ,
+                'review_data' : serializers.data
+            }
+            return Response( respone_data , status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return Response( {'error' : str(error)} , status=status.HTTP_400_BAD_REQUEST)
+        
+    def get(self ,request):
+        try:
+            reviewList = Review.objects.all()
+            serializers = ReviewSerializer(reviewList , many = True)
+            return Response(serializers.data , status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return Response( {'error' : str(error)} , status=status.HTTP_400_BAD_REQUEST)
+        
+    def put(self , request , pk):
+        try:
+            review_id = Review.objects.get(pk=pk)
+            print(request.user)
+            print(pk)
+            
+            serializers = ReviewSerializer(review_id, data=request.data ,partial = True)
+            
+            serializers.is_valid(raise_exception=True)
+            serializers.save()
+            respone_data = {
+                    'message' : 'Review Update successfully.' ,
                     'review_data' : serializers.data
                 }
-                return Response( respone_data , status=status.HTTP_201_CREATED)
-            return Response(serializers.errors , status=status.HTTP_400_BAD_REQUEST)
+            return Response( respone_data , status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return Response( {'error' : str(error)} , status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self ,request ,  pk):
+        try:
+            review_id = Review.objects.get(pk=pk)
+            print(pk)
+            review_id.delete()
+            return Response({'Delete Successfully'} , status=status.HTTP_204_NO_CONTENT)
+        except Review.DoesNotExist:
+            return Response({'error': 'Review not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_400_BAD_REQUEST)
+
+            
+            
