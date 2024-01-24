e3fc5bc200e4f96d0a5aebf6ceb52f79a7c9475b
hbheng <hb988651@gmail.com>
2024-01-24 17:53:40 +0700

    done add card

--- backend/derleng/urls.py
+++ backend/derleng/urls.py
@@ -6,10 +6,15 @@ from .views import ThumbnailView, PackageView, ReviewView, CategoryView , CartVi
 
 from rest_framework.routers import DefaultRouter
 
+from django.views.decorators.csrf import csrf_exempt
+
+
 viewsetRouter = DefaultRouter()
 viewsetRouter.register(r'packages', PackageView.PackageViewSet, basename='package')
 
 urlpatterns = [
+    path('cart', CartView.CartAPIView.as_view(), name='cart'),
+    path('cart/<uuid:pk>', CartView.CartAPIView.as_view(), name='cart'),
     path('', include(viewsetRouter.urls), name='viewset'),
     path('thumbnails' , ThumbnailView.ThumbnailAPIView.as_view() , name=''),
     path('thumbnails/<uuid:pk>' , ThumbnailView.ThumbnailAPIView.as_view() , name=''),
@@ -18,6 +23,5 @@ urlpatterns = [
     path('reviews' , ReviewView.ReviewAPIView.as_view(), name=''),
     path('reviews/<uuid:pk>' , ReviewView.ReviewAPIView.as_view(), name=''),
     path('test-payment/', Payment_methodView.test_payment, name="test_payment"),
-    path('services' , CartView.CartAPIView.as_view , name=''),
     path('payments', Payment_methodView.Payment_methodAPIView.as_view(), name='payment_method')
 ]
--- backend/derleng/views/CartView.py
+++ backend/derleng/views/CartView.py
@@ -1,46 +1,62 @@
-
-# from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from derleng.models import Cart , Package_service
+from derleng.models import Cart, Package_service
 from derleng.serializers import CartSerializer
 from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.csrf import csrf_exempt
 
 
 class CartAPIView(APIView):
     permission_classes = [IsAuthenticated]
+    @csrf_exempt
     def post(self, request):
         try:
-            # user = request.user  # Assuming you have authentication set up
-            # service_id = Package_service.objects.get(id=id)
-            # print(service_id)
-            # customer_amount = request.data.get('customer_amount', 1)
-            # booking_date = request.data.get('booking_date')
+            user = request.user
             data = request.data.copy()
-            print(data)
-            # data['user'] = user.id
-            # serializers = CartSerializer(data=data)
-            # serializers.is_valid(raise_exception=True)
-            # serializers.save()
-            
-            # Get the service
-            # service = Cart.objects.get(id=service_id)
+            data['user'] = user.id
+            serializers = CartSerializer(data=data)
+            serializers.is_valid(raise_exception=True)
+            serializers.save()
 
-            # # Create a new item in the cart
-            # cart_item = Cart.objects.create(
-            #     user=user,
-            #     service=service,
-            #     customer_amount=customer_amount,
-            #     booking_date=booking_date
-            # )
-
-            # Return a response with the added item details
-            # serializer = CartSerializer(cart_item)
             response_data = {
                 'message': 'Item added to cart successfully.',
-                # 'cart_item_data': serializers.data
+                'cart_item_data': serializers.data
             }
             return Response(response_data, status=status.HTTP_200_OK)
-        except Exception:
-            return Response({'error': 'Service not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_404_NOT_FOUND)
+        
+    def get(self , request):
+        try:
+            cartList = Cart.objects.all()
+            serializer = CartSerializer(cartList , many = True)
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response( {'error' : str(error)} , status=status.HTTP_400_BAD_REQUEST)
+
+    def put (self , request , pk ):
+        try:
+            cart_id = Cart.objects.get(pk=pk)
+            serializer = CartSerializer(cart_id , data=request.data , partial = True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            
+            respone_data = {
+                'message' : 'Cart update Successfully',
+                'data' : serializer.data
+            }
+            return Response(respone_data , status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return Response( {'error' : str(error)} , status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self , request, pk):
+        try:
+            cart_id = Cart.objects.get(pk=pk)
+            cart_id.delete()
+            return Response({'Delete Successfully'} , status=status.HTTP_204_NO_CONTENT)
+        except Cart.DoesNotExist:
+            return Response({'error': 'Cart not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_400_BAD_REQUEST)
+        
